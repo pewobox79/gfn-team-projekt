4412b06e0fb10ac9460a7b11697181765e9dbb8c
Hamza <hamza.tuto15@gmail.com>
2026-02-20 09:38:21 +0100

    call to action added

--- frontend-vite/src/pages/HomePage.tsx
+++ frontend-vite/src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import CallToAction from "@/components/CallToActionElement"
 export default function HomePage() {
 
     return <>
-        <h1 className="text-[4rem]">GFN Projekt</h1>
+        <h1 className="text-[4rem] ">GFN Projekt</h1>
         <CallToAction 
             title="Subscribe to our Newsletter!"
         />

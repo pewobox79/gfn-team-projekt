008e7af0eff06fb6f53a1ef16c8710e5153d9f43
ElnazTayebi <elnazt67@gmail.com>
2026-02-25 12:32:37 +0100

    add Mobil menu

--- frontend-vite/src/layouts/MainLayout.tsx
+++ frontend-vite/src/layouts/MainLayout.tsx
@@ -1,8 +1,10 @@
+import MobilMenu from "@/components/header/MobilMenu";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout(){
 
     return <div className="w-full p-4 flex center flex-col items-center">
+        <MobilMenu />
         <Outlet/>
     </div>
 }

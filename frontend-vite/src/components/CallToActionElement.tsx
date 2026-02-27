d4a21109cdc7b270418017caf674ae8a374a3e96
Hamza <hamza.tuto15@gmail.com>
2026-02-27 22:56:30 +0100

    CTA last update following STRAPI structure

--- frontend-vite/src/components/CallToActionElement.tsx
+++ frontend-vite/src/components/CallToActionElement.tsx
@@ -1,69 +1,48 @@
-import { useState } from "react";
-import { Send } from "lucide-react";
+
+import MyButton, { type MyButtonProps } from "./MyButton";
 
 type CallToActionProps = {
   title: string;
-  placeholder?: string;
-  buttonText?: string;
+  description: string
   bgColor?: string;
+  hasButton?: boolean
+  button?: MyButtonProps
 };
 
 export default function CallToAction({
   title,
-  placeholder = "your Email",
-  buttonText = "Subscribe Now",
-  bgColor = "bg-slate-800",
-}: CallToActionProps) {
-  const [email, setEmail] = useState("");
-  const [status, setStatus] = useState<"idle" | "success">("idle");
-
-  const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-
-  function handleSubscribe() {
-    setStatus("success");
-    setEmail("");
+  description = 'this description lorem ipsum was brought to you by blabla hirone the third company in a row.',
+  bgColor = "black",
+  hasButton= true,
+  button={
+    label:'Subscribe Now!',
+    link:'',
+    bgColor : "red",
+    hasBorderRadius: true
   }
+}: CallToActionProps) {
+
+  const getBg = () => {
+  if (bgColor === "red") return "bg-red-600";
+  if (bgColor === "black") return "bg-black";
+  if (bgColor === "white") return "bg-white";
+  return "bg-black";
+};
 
   return (
-    <div className={`w-full rounded-2xl p-6 ${bgColor}`}>
+    <div className={`w-full rounded-2xl p-6 ${getBg()}`}>
       {/* Title */}
-      <h2 className="text-white text-lg font-semibold">{title}</h2>
+      
 
       {/* Input where to give your email + Button*/}
-      <div className="flex w-full md:w-auto gap-2">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between w-full gap-4">
+        <h2 className="text-white text-lg font-semibold whitespace-nowrap">{title}</h2>
+        <p className="text-white">{description}</p>
 
-        <input
-          type="email"
-          placeholder={placeholder}
-          className="flex-1 md:w-72
-            rounded-lg px-4 py-2
-            bg-white/10
-            text-white placeholder-white/60
-            outline-none focus:ring-2 focus:ring-white/40"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
+        {/* Button */}
+        {hasButton && <MyButton {...button}/>}
+        
 
-        {/* Button only looks enabled when email is valid */}
-        <button
-          className={`flex items-center gap-2 px-4 py-2 rounded-lg font-medium transition text-white whitespace-nowrap shrink-0
-            ${isValid
-                ? "bg-red-500 hover:bg-red-600"
-                : "bg-red-500/60 cursor-not-allowed pointer-events-none"
-                }
-            `}
-          onClick={handleSubscribe}
-          disabled={!isValid}
-        >
-          <span>{buttonText}</span>
-          <Send size={18} className="shrink-0" />
-        </button>
-
-        {status === "success" && (
-          <p className="text-green-300 text-sm mt-2">Thanks for subscribing!</p>
-        )}
       </div>
     </div>
   );

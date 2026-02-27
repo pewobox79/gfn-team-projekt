9e789e62f1d94c94a99bcbd3a56322ca24e87cec
Sindy-h <sindy1990h@gmail.com>
2026-02-27 12:32:16 +0100

    function getOptimizedImage created

---
+++ frontend-vite/src/utils/formatter.ts
@@ -0,0 +1,34 @@
+function createImgUrl(urlString: string) {
+    const baseUrl = "http://localhost:1337"
+    return baseUrl + urlString
+
+
+}
+
+function getOptimizedImage(imageObject: any) {
+
+    if (!imageObject) 
+        
+        return {
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWCQV0JduDI9swIp_Oy32i_omTZAnoYMPR4A&s",
+            alt: "Lucky Cat"
+        }
+        
+
+    const alt = imageObject?.alternativeText || "mein Bild"
+
+    if (imageObject.formats?.medium?.url) {
+
+        return {
+            url: createImgUrl(imageObject.formats.medium.url),
+            alt
+        }
+    }
+
+    return {
+        url: createImgUrl(imageObject.url),
+        alt
+    }
+}
+
+export default getOptimizedImage

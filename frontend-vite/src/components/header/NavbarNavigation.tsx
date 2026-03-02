750196eed57b814d696180e11718b5f323b45d46
ElnazTayebi <elnazt67@gmail.com>
2026-03-02 14:08:22 +0100

    text black styling

--- frontend-vite/src/components/header/NavbarNavigation.tsx
+++ frontend-vite/src/components/header/NavbarNavigation.tsx
@@ -25,7 +25,7 @@ const NavbarNavigation = ({ onClick }: { onClick?: () => void }) => {
                 <Link key={item.id + item.label}
                     to={item.link}
                     onClick={onClick}
-                    className="block md:inline text-block hover:text-orange-500 font-medium text-lg transition px-6 md:px-0 py-4 md:py-0"
+                    className="block text-black md:inline hover:text-orange-500 font-medium text-lg transition px-6 md:px-0 py-4 md:py-0"
                 >
                     {item.label}
                 </Link>

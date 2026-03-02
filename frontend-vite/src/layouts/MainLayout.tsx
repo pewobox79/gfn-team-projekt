3713344bca3303d6ad3b13ba7bd66959226402dd
ElnazTayebi <elnazt67@gmail.com>
2026-03-02 11:43:32 +0100

    navbar added

--- frontend-vite/src/layouts/MainLayout.tsx
+++ frontend-vite/src/layouts/MainLayout.tsx
@@ -1,10 +1,6 @@
-import MobilMenu from "@/components/header/Navbar";
 import { Outlet } from "react-router-dom";
-
 export default function MainLayout(){
-
     return <div className="w-full p-4 flex center flex-col items-center">
-        <MobilMenu />
         <Outlet/>
     </div>
 }
